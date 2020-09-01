9c675ebd4cdcf13d57f3a91c5370a02e87437d6e
BrunoEspindolaWork <brunoespindola.work@gmail.com>
2020-09-01 20:45:47 -0300

    version-1.3 - sounds repair

--- js/sounds.js
+++ js/sounds.js
@@ -5,26 +5,26 @@
 
 // Mouse Click
  var mouseClick = document.createElement("audio");
- mouseClick.setAttribute("src", "../assets/audio/mouseClick.mp3");
+ mouseClick.setAttribute("src", "https://brunoespindolawork.github.io/assets/audio/mouseCick.mp3");
  mouseClick.setAttribute("autoplay:false", "autoplay");
 
 // Success
  var success = document.createElement("audio");
- success.setAttribute("src", "../assets/audio/success.mp3");
+ success.setAttribute("src", "https://brunoespindolawork.github.io/assets/audio/success.mp3");
  success.setAttribute("autoplay:false", "autoplay");
 
 // Fail
  var fail = document.createElement("audio");
- fail.setAttribute("src", "../assets/audio/fail.mp3");
+ fail.setAttribute("src", "https://brunoespindolawork.github.io/assets/audio/fail.mp3");
  fail.setAttribute("autoplay:false", "autoplay");
  
  // Select
  var select = document.createElement("audio");
- select.setAttribute("src", "../assets/audio/select.mp3");
+ select.setAttribute("src", "https://brunoespindolawork.github.io/assets/audio/select.mp3");
  select.setAttribute("autoplay:false", "autoplay");
  
   // Winner
   var winner = document.createElement("audio");
-  winner.setAttribute("src", "../assets/audio/winner.mp3");
+  winner.setAttribute("src", "https://brunoespindolawork.github.io/assets/audio/winner.mp3");
   winner.setAttribute("autoplay:false", "autoplay");
   
